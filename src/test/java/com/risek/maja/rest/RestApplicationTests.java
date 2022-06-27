6e12cfe54830273bd53ad494f181931ed6bc941a
Maja Risek <maja.risek1@gmail.com>
2022-06-27 22:19:15 +0200

    hello spring

---
+++ src/test/java/com/risek/maja/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.risek.maja.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
